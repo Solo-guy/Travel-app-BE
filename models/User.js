7465b272377f3d6a9ca4c914d2333a2a6ad65977
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2023-10-25 11:29:25 +0700

    day2

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: {type: String, require: true},
     email: {type: String, require: true, unique: true },
     password: {type: String, require: true},
-    profile: {type: String, default: true},
+    profile: {type: String, default: "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="},
 })
 
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const errorHandler = require('./middleware/errorHandling')
+const authRouter = require('./routes/auth');    
 const port = 5003
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('database conected'))
 .catch((err) => console.log(err))
+
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb', extended: true}));
+
+app.use(errorHandler);
+app.use('/api/', authRouter);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
